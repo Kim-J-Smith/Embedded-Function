80d38c2d83f8f085b7c907f1a09e0d55477624f9
Kim-J-Smith <3440910457@qq.com>
2026-03-06 00:32:50 +0800

    test: add more tests.

---
+++ test/test_Swap.cpp
@@ -0,0 +1,14 @@
+#include "embed/embed_function.hpp"
+#include "gtest/gtest.h"
+#include "test_function.hpp"
+
+TEST(TestSwap, fn_swap) {
+    auto f1 = ebd::make_fn(ebd_test_free_func_iii_add);
+    decltype(f1) f2;
+
+    f2.swap(f1);
+
+    ASSERT_EQ(f1 == nullptr, true);
+    ASSERT_EQ(f2 == nullptr, false);
+    ASSERT_EQ(f2(3, 4), 3 + 4);
+}
