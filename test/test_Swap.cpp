33f527e95395f0e91eb6be867051be5a9ef0c75a
Kim-J-Smith <3440910457@qq.com>
2026-03-07 21:17:29 +0800

    test: add swap() tests.

--- test/test_Swap.cpp
+++ test/test_Swap.cpp
@@ -12,3 +12,36 @@ TEST(TestSwap, fn_swap) {
     ASSERT_EQ(f2 == nullptr, false);
     ASSERT_EQ(f2(3, 4), 3 + 4);
 }
+
+TEST(TestSwap, fn_view_swap) {
+    auto f1 = ebd::fn_view<int(int, int)>(ebd_test_free_func_iii_add);
+    decltype(f1) f2;
+
+    f2.swap(f1);
+
+    ASSERT_EQ(f1 == nullptr, true);
+    ASSERT_EQ(f2 == nullptr, false);
+    ASSERT_EQ(f2(3, 4), 3 + 4);
+}
+
+TEST(TestSwap, fn_view_swap) {
+    auto f1 = ebd::safe_fn<int(int, int)>(ebd_test_free_func_iii_add);
+    decltype(f1) f2;
+
+    f2.swap(f1);
+
+    ASSERT_EQ(f1 == nullptr, true);
+    ASSERT_EQ(f2 == nullptr, false);
+    ASSERT_EQ(f2(3, 4), 3 + 4);
+}
+
+TEST(TestSwap, unique_fn_swap) {
+    auto f1 = ebd::unique_fn<int(int, int)>(ebd_test_free_func_iii_add);
+    decltype(f1) f2;
+
+    f2.swap(f1);
+
+    ASSERT_EQ(f1 == nullptr, true);
+    ASSERT_EQ(f2 == nullptr, false);
+    ASSERT_EQ(f2(3, 4), 3 + 4);
+}
