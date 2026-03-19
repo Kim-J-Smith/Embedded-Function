eeb43a0a074ea6c53c6f9b3b80dcf6a43723c103
Kim-J-Smith <3440910457@qq.com>
2026-03-20 00:44:09 +0800

    feat: big buffer should bigger than sizeof(std::function) to wrap the std::function object.

--- include/embed/embed_function.hpp
+++ include/embed/embed_function.hpp
@@ -1023,7 +1023,9 @@ inline namespace fn_traits {
 #if defined(EMBED_FN_CONFIG_USE_BIG_DEFAULT_BUFFER)
     // The CommandTable size plus the buffer size is about 8 * sizeof(void).
     // TODO: The size of this buffer zone needs further examination.
-    static constexpr std::size_t value = 6 * sizeof(void*);
+    static constexpr std::size_t value_c1 = 6 * sizeof(void*);
+    static constexpr std::size_t value_c2 = sizeof(::std::function<void()>);
+    static constexpr std::size_t value = value_c1 > value_c2 ? value_c1 : value_c2;
 #else
     static constexpr std::size_t value = sizeof(void (UndefinedClass::*) ());
 #endif
