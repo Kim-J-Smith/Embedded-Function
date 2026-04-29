324cb4c08095f78c5a97bfe59b574b4ca1fdf46b
Kim-J-Smith <3440910457@qq.com>
2026-04-29 18:07:36 +0800

    test: add conformance test for ebd::fn_ref.

---
+++ test/conformance/fn_ref/assign.delete.pass.cpp
@@ -0,0 +1,126 @@
+//===----------------------------------------------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+// Additional modifications:
+// Copyright (c) 2026 Kim-J-Smith
+// SPDX-License-Identifier: MIT
+// 
+//===----------------------------------------------------------------------===//
+
+// REQUIRES: C++11 ~ C++26
+
+// Constraints:
+// - is-convertible-from-specialization<T> is false,
+// - is_pointer_v<T> is false, and
+// - T is not a specialization of constant_wrapper. (Unused for `ebd::fn_ref`)
+
+#include <functional>
+#include <utility>
+#include <type_traits>
+
+#include "embed/embed_function.hpp"
+#include "gtest/gtest.h"
+
+#include "test_function.hpp"
+
+#define STATIC_ASSERT_(...) static_assert((__VA_ARGS__), "BUG")
+#define ASSERT_(...) ASSERT_EQ((__VA_ARGS__) == true, true)
+
+// non const noexcept(false)
+STATIC_ASSERT_(std::is_assignable<ebd::fn_ref<void()>, ebd::fn_ref<void()>>::value);
+STATIC_ASSERT_(std::is_assignable<ebd::fn_ref<void()>, ebd::fn_ref<void() const>>::value);
+#if __cpp_noexcept_function_type >= 201510L
+  STATIC_ASSERT_(std::is_assignable<ebd::fn_ref<void()>, ebd::fn_ref<void() noexcept>>::value);
+  STATIC_ASSERT_(std::is_assignable<ebd::fn_ref<void()>, ebd::fn_ref<void() const noexcept>>::value);
+#endif
+
+STATIC_ASSERT_(std::is_assignable<ebd::fn_ref<void()>, void (*)()>::value);
+STATIC_ASSERT_(!std::is_assignable<ebd::fn_ref<void()>, void (*)(int)>::value);
+
+// STATIC_ASSERT_(std::is_assignable<ebd::fn_ref<void()>, std::constant_wrapper<[] {}>>::value);
+// STATIC_ASSERT_(!std::is_assignable<ebd::fn_ref<void()>, std::constant_wrapper<[](int) {}>>::value);
+
+// const noexcept(false)
+STATIC_ASSERT_(std::is_assignable<ebd::fn_ref<void() const>, ebd::fn_ref<void()>>::value);
+STATIC_ASSERT_(std::is_assignable<ebd::fn_ref<void() const>, ebd::fn_ref<void() const>>::value);
+#if __cpp_noexcept_function_type >= 201510L
+  STATIC_ASSERT_(std::is_assignable<ebd::fn_ref<void() const>, ebd::fn_ref<void() noexcept>>::value);
+  STATIC_ASSERT_(std::is_assignable<ebd::fn_ref<void() const>, ebd::fn_ref<void() const noexcept>>::value);
+#endif
+
+STATIC_ASSERT_(std::is_assignable<ebd::fn_ref<void() const>, void (*)()>::value);
+STATIC_ASSERT_(!std::is_assignable<ebd::fn_ref<void() const>, void (*)(int)>::value);
+
+// STATIC_ASSERT_(std::is_assignable<ebd::fn_ref<void() const>, std::constant_wrapper<[] { return 42; }>>::value);
+// STATIC_ASSERT_(!std::is_assignable<ebd::fn_ref<void() const>, std::constant_wrapper<[](int) { return 42; }>>::value);
+
+// non-const noexcept(true)
+#if __cpp_noexcept_function_type >= 201510L
+  STATIC_ASSERT_(!std::is_assignable<ebd::fn_ref<void() noexcept>, ebd::fn_ref<void()>>::value);
+  STATIC_ASSERT_(!std::is_assignable<ebd::fn_ref<void() noexcept>, ebd::fn_ref<void() const>>::value);
+  STATIC_ASSERT_(std::is_assignable<ebd::fn_ref<void() noexcept>, ebd::fn_ref<void() noexcept>>::value);
+  STATIC_ASSERT_(std::is_assignable<ebd::fn_ref<void() noexcept>, ebd::fn_ref<void() const noexcept>>::value);
+
+  STATIC_ASSERT_(std::is_assignable<ebd::fn_ref<void() noexcept>, void (*)() noexcept>::value);
+  STATIC_ASSERT_(!std::is_assignable<ebd::fn_ref<void() noexcept>, void (*)(int) noexcept>::value);
+
+  // STATIC_ASSERT_(std::is_assignable<ebd::fn_ref<void() noexcept>, std::constant_wrapper<[] noexcept {} >>);
+  // STATIC_ASSERT_(!std::is_assignable<ebd::fn_ref<void() noexcept>, std::constant_wrapper<[](int) noexcept {}>>);
+
+  // const noexcept(true)
+  STATIC_ASSERT_(!std::is_assignable<ebd::fn_ref<void() const noexcept>, ebd::fn_ref<void()>>::value);
+  STATIC_ASSERT_(!std::is_assignable<ebd::fn_ref<void() const noexcept>, ebd::fn_ref<void() const>>::value);
+  STATIC_ASSERT_(std::is_assignable<ebd::fn_ref<void() const noexcept>, ebd::fn_ref<void() noexcept>>::value);
+  STATIC_ASSERT_(std::is_assignable<ebd::fn_ref<void() const noexcept>, ebd::fn_ref<void() const noexcept>>::value);
+
+  STATIC_ASSERT_(std::is_assignable<ebd::fn_ref<void() const noexcept>, void (*)() noexcept>::value);
+  STATIC_ASSERT_(!std::is_assignable<ebd::fn_ref<void() const noexcept>, void (*)(int) noexcept>::value);
+
+  // STATIC_ASSERT_(std::is_assignable<ebd::fn_ref<void() const noexcept>, std::constant_wrapper<[] noexcept {}>>::value);
+  // STATIC_ASSERT_(
+  //     !std::is_assignable<ebd::fn_ref<void() const noexcept>, std::constant_wrapper<[](int) noexcept {}>>::value);
+#endif
+
+int forty_two() { return 42; }
+
+TEST(Conformance_fn_ref, assign_delete_pass) {
+#if 0 && __cpp_lib_function_ref >= 202603L
+  {
+    ebd::fn_ref<int()> f(std::cw<[] { return 41; }>);
+    f = ebd::fn_ref<int()>(std::cw<[] { return 42; }>);
+    ASSERT_(f() == 42);
+  }
+  {
+    ebd::fn_ref<int() > f(std::cw<[] { return 41; }>);
+    f = &forty_two;
+    ASSERT_(f() == 42);
+  }
+  {
+    ebd::fn_ref<int() > f(std::cw<[] { return 41; }>);
+    f = std::cw<[] { return 42; }>;
+    ASSERT_(f() == 42);
+  }
+  {
+    // const
+    ebd::fn_ref<int() const> f(std::cw<[] { return 41; }>);
+    f = std::cw<[] { return 42; }>;
+    ASSERT_(f() == 42);
+  }
+  {
+    // noexcept
+    ebd::fn_ref<int() noexcept> f(std::cw<[] noexcept { return 41; }>);
+    f = std::cw<[] noexcept { return 42; }>;
+    ASSERT_(f() == 42);
+  }
+  {
+    // const noexcept
+    ebd::fn_ref<int() const noexcept> f(std::cw<[] noexcept { return 41; }>);
+    f = std::cw<[] noexcept { return 42; }>;
+    ASSERT_(f() == 42);
+  }
+#endif
+  SUCCEED();
+}
---
+++ test/conformance/fn_ref/copy.pass.cpp
@@ -0,0 +1,140 @@
+//===----------------------------------------------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+// Additional modifications:
+// Copyright (c) 2026 Kim-J-Smith
+// SPDX-License-Identifier: MIT
+// 
+//===----------------------------------------------------------------------===//
+
+// REQUIRES: C++11 ~ C++26
+
+// constexpr fn_ref(const fn_ref&) noexcept = default;
+
+#include <functional>
+#include <utility>
+#include <type_traits>
+
+#include "embed/embed_function.hpp"
+#include "gtest/gtest.h"
+
+#include "test_function.hpp"
+
+#define STATIC_ASSERT_(...) static_assert((__VA_ARGS__), "BUG")
+#define ASSERT_(...) ASSERT_EQ((__VA_ARGS__) == true, true)
+#ifndef TEST_IS_CONSTANT_EVALUATED
+# define TEST_IS_CONSTANT_EVALUATED 0
+#endif
+
+STATIC_ASSERT_(std::is_copy_constructible<ebd::fn_ref<void()>>::value);
+STATIC_ASSERT_(std::is_copy_constructible<ebd::fn_ref<void() const>>::value);
+#if __cpp_noexcept_function_type >= 201510L
+  STATIC_ASSERT_(std::is_copy_constructible<ebd::fn_ref<void() noexcept>>::value);
+  STATIC_ASSERT_(std::is_copy_constructible<ebd::fn_ref<void() const noexcept>>::value);
+#endif
+
+double f1(int x, double y) noexcept { return x + y; }
+
+struct Int {
+  int i;
+  constexpr Int(int ii) noexcept : i(ii) {}
+};
+
+struct NeedsConversion {
+  int operator()(Int x, Int y, Int z) const noexcept { return x.i + y.i + z.i; }
+};
+
+int needs_conversion(Int x, Int y, Int z) noexcept { return x.i + y.i + z.i; }
+
+TEST(Conformance_fn_ref, copy_pass) {
+#if 0 && __cpp_lib_function_ref >= 202603L
+  {
+    ebd::fn_ref<void()> f(std::cw<[] {}>);
+    auto f2 = f;
+    if (!TEST_IS_CONSTANT_EVALUATED) {
+      f2();
+    }
+  }
+  {
+    const
+    ebd::fn_ref<int() const> f(std::cw<[] { return 42; }>);
+    auto f2 = f;
+
+    if (!TEST_IS_CONSTANT_EVALUATED) {
+      ASSERT_(f2() == 42);
+    }
+  }
+  {
+    // noexcept
+    ebd::fn_ref<double(int, double) noexcept> f(std::cw<&f1>);
+    auto f2 = f;
+    if (!TEST_IS_CONSTANT_EVALUATED) {
+      ASSERT_(f2(1, 2.0) == 3.0);
+    }
+  }
+  {
+    // const noexcept
+    ebd::fn_ref<double(int, double) const noexcept> f(std::cw<&f1>);
+    auto f2 = f;
+    if (!TEST_IS_CONSTANT_EVALUATED) {
+      ASSERT_(f2(1, 2.0) == 3.0);
+    }
+  }
+  {
+    // with conversions
+    ebd::fn_ref<Int(int, int, int)> f(std::cw<NeedsConversion{}>);
+    auto f_copy = f;
+    if (!TEST_IS_CONSTANT_EVALUATED) {
+      ASSERT_(f_copy(1, 2, 3).i == 6);
+    }
+
+    ebd::fn_ref<Int(int, int, int) const> f2(std::cw<NeedsConversion{}>);
+    auto f2_copy = f2;
+    if (!TEST_IS_CONSTANT_EVALUATED) {
+      ASSERT_(f2_copy(1, 2, 3).i == 6);
+    }
+
+    ebd::fn_ref<Int(int, int, int) noexcept> f3(std::cw<NeedsConversion{}>);
+    auto f3_copy = f3;
+    if (!TEST_IS_CONSTANT_EVALUATED) {
+      ASSERT_(f3_copy(1, 2, 3).i == 6);
+    }
+
+    ebd::fn_ref<Int(int, int, int) const noexcept> f4(std::cw<NeedsConversion{}>);
+    auto f4_copy = f4;
+    if (!TEST_IS_CONSTANT_EVALUATED) {
+      ASSERT_(f4_copy(1, 2, 3).i == 6);
+    }
+  }
+  {
+    // with conversions function pointer
+    ebd::fn_ref<Int(int, int, int)> f(std::cw<&needs_conversion>);
+    auto f_copy = f;
+    if (!TEST_IS_CONSTANT_EVALUATED) {
+      ASSERT_(f_copy(1, 2, 3).i == 6);
+    }
+
+    ebd::fn_ref<Int(int, int, int) const> f2(std::cw<&needs_conversion>);
+    auto f2_copy = f2;
+    if (!TEST_IS_CONSTANT_EVALUATED) {
+      ASSERT_(f2_copy(1, 2, 3).i == 6);
+    }
+
+    ebd::fn_ref<Int(int, int, int) noexcept> f3(std::cw<&needs_conversion>);
+    auto f3_copy = f3;
+    if (!TEST_IS_CONSTANT_EVALUATED) {
+      ASSERT_(f3_copy(1, 2, 3).i == 6);
+    }
+
+    ebd::fn_ref<Int(int, int, int) const noexcept> f4(std::cw<&needs_conversion>);
+    auto f4_copy = f4;
+    if (!TEST_IS_CONSTANT_EVALUATED) {
+      ASSERT_(f4_copy(1, 2, 3).i == 6);
+    }
+  }
+#endif
+  SUCCEED();
+}
