6c9be5b78ff7f8c3406f28ecf2293aa6b0de11ae
Kim-J-Smith <3440910457@qq.com>
2026-05-08 23:21:26 +0800

    test: add more test cases.

--- test/conformance/fn_ref/conformance.addition.pass.cpp
+++ test/conformance/fn_ref/conformance.addition.pass.cpp
@@ -4,6 +4,8 @@
 
 #include "__constant_wrapper.hpp"
 
+#define EMBED_FN_HOOK_DEBUG(msg) // puts(msg)
+
 #include "embed/embed_function.hpp"
 #include "gtest/gtest.h"
 
@@ -11,6 +13,12 @@
 
 template <class Sig, std::size_t Buf>
 using nothrow_fn = ebd::basic_fn<Sig, Buf, true, false, false, false>;
+
+static std::reference_wrapper<ebd_test_member_fn> get_ref() {
+  static ebd_test_member_fn x;
+  x.member_var = 0;
+  return x;
+}
 
 TEST(Conformance_fn_ref, conformance_addition_pass) {
 
@@ -52,11 +60,60 @@ TEST(Conformance_fn_ref, conformance_addition_pass) {
     static_assert(std::is_same<decltype(f2), nothrow_fn<int() const noexcept, sizeof(void(*)())>>::value, "BUG");
     ASSERT_EQ(f2(), 0);
   }
+
+  {
+    // https://www.open-std.org/jtc1/sc22/wg21/docs/papers/2026/p3961r1.html
+    ebd::fn_ref<int() noexcept> f_noexcept = ebd_test_free_func_noexcept;
+    ebd::fn_ref<int() noexcept(false)> f_maythrow = ebd_test_free_func_maythrow;
+
+    ASSERT_EQ(f_noexcept(), 0);
+    ASSERT_EQ(f_maythrow(), 1);
+
+    f_maythrow = f_noexcept;
+    ASSERT_EQ(f_maythrow(), 0); // call ebd_test_free_func_noexcept
+    // f_noexcept = f_maythrow; // ERROR
+
+    f_noexcept = ebd_test_free_func_noexcept_2;
+    ASSERT_EQ(f_noexcept(), 2);
+    ASSERT_EQ(f_maythrow(), 0); // call ebd_test_free_func_noexcept
+                                // It should not be affected by f_noexcept.
+  }
 #endif
 
   {
     // volatile is still supported.
     volatile ebd::fn_ref<void() volatile> f1 = +[] { volatile int a = 1; (void)a; };
     f1();
+
+    volatile ebd_test_operator_qualifier obj{};
+    ebd::fn_ref<int(int) volatile> f2 = obj;
+    ASSERT_EQ(f2(0), OVL_VOLATILE);
   }
+
+  {
+    static_cast<void>(&get_ref); // unused
+  }
+
+#if __cpp_lib_constant_wrapper >= 202603L
+
+  {
+    auto rx = get_ref();
+    ebd::fn_ref<int(int)> f1(std::cw<&ebd_test_member_fn::get_var_and_increase>, rx);
+
+    // Should this be allowed? get_var_and_increase() is not `const`.
+    // See https://www.open-std.org/jtc1/sc22/wg21/docs/papers/2026/p3961r1.html#Why-stop-at-function_ref-as-opposed-to-extending-to-reference_wrapper .
+    ebd::fn_ref<int(int) const> f2(std::cw<&ebd_test_member_fn::get_var_and_increase>, rx);
+  }
+
+  {
+#if !defined(NDEBUG)
+    ASSERT_DEATH({ 
+      ebd::fn_ref<int(int)> f1(
+        std::cw<&ebd_test_member_fn::get_var_and_increase>, 
+        static_cast<ebd_test_member_fn*>(nullptr));
+    }, "");
+#endif
+  }
+
+#endif
 }
