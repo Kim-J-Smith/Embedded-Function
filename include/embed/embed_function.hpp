58174b19b0af20e5fa741b0d5aeab2e0fa8d8ca1
Kim-J-Smith <3440910457@qq.com>
2026-07-14 19:41:02 +0800

    chore: delete useless config macro EMBED_FN_CONFIG_USE_BIG_DEFAULT_BUFFER.

--- include/embed/embed_function.hpp
+++ include/embed/embed_function.hpp
@@ -16,9 +16,6 @@
 
 // Just like function pointers, it is quick and efficient.
 
-/// @b EMBED_FN_CONFIG_USE_BIG_DEFAULT_BUFFER
-/// If this macro is defined, bigger default buffer size will be used.
-
 /// @b EMBED_FN_CONFIG_DISABLE_SMART_FORWARD
 /// If this macro is defined, `smart_forward_t` will fall back to Perfect Forwarding.
 
@@ -1148,16 +1145,7 @@ inline namespace fn_traits {
     // The buffer size for ebd::fn_ref. Stop supporting pointer-to-members.
     static constexpr std::size_t ref_buf = sizeof(void (*) ());
     static constexpr std::size_t view_buf = ref_buf;
-#if defined(EMBED_FN_CONFIG_USE_BIG_DEFAULT_BUFFER)
-    // The CommandTable size plus the buffer size is about 8 * sizeof(void*).
-    // TODO: The size of this buffer zone needs further examination.
-    static constexpr std::size_t value_c1 = 6 * sizeof(void*);
-    static constexpr std::size_t value_c2 = sizeof(::std::function<void()>);
-    static constexpr std::size_t value = value_c1 > value_c2 ? value_c1 : value_c2;
-#else
     static constexpr std::size_t value = sizeof(void (UndefinedClass::*) ());
-#endif
-
     static constexpr std::size_t align_value = alignof(void (UndefinedClass::*) ());
   };
 
@@ -3546,7 +3534,6 @@ namespace detail {
 # undef EMBED_NODISCARD
 # undef EMBED_DEPRECATED
 
-# undef EMBED_FN_CONFIG_USE_BIG_DEFAULT_BUFFER
 # undef EMBED_FN_CONFIG_DISABLE_SMART_FORWARD
 # undef EMBED_FN_CONFIG_UNDEF_MACROS
 # undef EMBED_FN_HOOK_DEBUG
