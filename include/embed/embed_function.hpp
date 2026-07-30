5f1f2e470f637f83bba6a2353af5c7e44ef0cd9c
Kim-J-Smith <3440910457@qq.com>
2026-07-30 21:46:41 +0800

    chore: remove macro EMBED_FN_CONFIG_USE_BIG_DEFAULT_BUFFER.

--- include/embed/embed_function.hpp
+++ include/embed/embed_function.hpp
@@ -16,9 +16,6 @@
 
 // Just like function pointers, it is quick and efficient.
 
-/// @deprecated @b EMBED_FN_CONFIG_USE_BIG_DEFAULT_BUFFER
-/// If this macro is defined, bigger default buffer size will be used.
-
 /// @b EMBED_FN_CONFIG_DISABLE_SMART_FORWARD
 /// If this macro is defined, `smart_forward_t` will fall back to Perfect Forwarding.
 
@@ -1119,14 +1116,7 @@ inline namespace fn_traits {
     // The buffer size for ebd::fn_ref. Stop supporting pointer-to-members.
     static constexpr std::size_t ref_buf = sizeof(void (*) ());
     static constexpr std::size_t view_buf = ref_buf;
-#if defined(EMBED_FN_CONFIG_USE_BIG_DEFAULT_BUFFER)
-    /// @deprecated @todo TODO: Remove this in version-2.2.0.
-    static constexpr std::size_t value_c1 = 6 * sizeof(void*);
-    static constexpr std::size_t value_c2 = sizeof(::std::function<void()>);
-    static constexpr std::size_t value = value_c1 > value_c2 ? value_c1 : value_c2;
-#else
     static constexpr std::size_t value = sizeof(void (UndefinedClass::*) ());
-#endif
 
     // The alignment for owning wrappers (ebd::fn, ebd::unique_fn, ebd::safe_fn).
     // Using alignof(std::max_align_t) ensures that objects with the maximum
@@ -3474,7 +3464,6 @@ namespace detail {
 # undef EMBED_NODISCARD
 # undef EMBED_DEPRECATED
 
-# undef EMBED_FN_CONFIG_USE_BIG_DEFAULT_BUFFER
 # undef EMBED_FN_CONFIG_DISABLE_SMART_FORWARD
 # undef EMBED_FN_CONFIG_UNDEF_MACROS
 # undef EMBED_FN_HOOK_DEBUG
