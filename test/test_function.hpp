636c5d67fbfcff2d97eb5c9461656523d44166eb
Kim-J-Smith <3440910457@qq.com>
2026-08-07 19:20:52 +0800

    test: add more std::in_place_type tests.

--- test/test_function.hpp
+++ test/test_function.hpp
@@ -250,6 +250,8 @@ struct ebd_test_counter {
     static int m_copy_times;
     static int m_move_times;
     static int m_delete_times;
+
+    ebd_test_counter(int) noexcept { m_create_times++; }
     ebd_test_counter() noexcept { m_create_times++; }
     ebd_test_counter(const ebd_test_counter&) noexcept { m_copy_times++; }
     ebd_test_counter(ebd_test_counter&&) noexcept { m_move_times++; }
