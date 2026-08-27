390e0430a861f3b5c38a8af9d3fe7f6a02ec5dfe
Kim-J-Smith <3440910457@qq.com>
2026-08-28 00:30:39 +0800

    test: add tests for new feature.

---
+++ test/conformance/fn/constant_wrapper.pass.cpp
@@ -0,0 +1,18 @@
+#include "__constant_wrapper.hpp"
+#include "test_function.hpp"
+
+#if __cpp_lib_constant_wrapper >= 202603L
+
+TEST(Conformance_fn, constant_wrapper_pass) {
+    {
+        using Class = ebd_test_member_fn;
+        ebd::fn<int(int, int)> f1(std::cw<&Class::mem_fn_ii_add>, Class{});
+        ASSERT_EQ(f1(0, 42), 42);
+        ASSERT_EQ(f1(1, 42), 43);
+
+        ebd::fn<int() noexcept> f2(std::cw<&Class::mem_fn_noexcept>, Class{});
+        ASSERT_EQ(f2(), 0);
+    }
+}
+
+#endif // __cpp_lib_constant_wrapper >= 202603L
