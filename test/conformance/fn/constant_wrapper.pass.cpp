7ee1cd187b496ccc603be018fbeb4bcbeead7dc2
Kim-J-Smith <3440910457@qq.com>
2026-09-02 13:28:25 +0800

    test: more tests for new feature.

--- test/conformance/fn/constant_wrapper.pass.cpp
+++ test/conformance/fn/constant_wrapper.pass.cpp
@@ -11,6 +11,10 @@ struct ExplicitThis {
     }
 };
 
+static int func_iii_add_noexcept(int a, int b) noexcept {
+    return a + b;
+}
+
 }
 
 
@@ -19,6 +23,11 @@ TEST(Conformance_fn, constant_wrapper_pass) {
         using Class = ebd_test_member_fn;
         Class obj{};
         auto* ptr = &obj;
+
+        static_assert(std::is_constructible_v<ebd::fn<int(int, int)>,
+            std::constant_wrapper<&Class::mem_fn_ii_add>, Class>);
+        static_assert(!std::is_nothrow_constructible_v<ebd::fn<int(int, int)>,
+            std::constant_wrapper<&Class::mem_fn_ii_add>, Class>);
 
         {
             ebd::fn<int(int, int)> f1(std::cw<&Class::mem_fn_ii_add>, obj);
@@ -42,6 +51,54 @@ TEST(Conformance_fn, constant_wrapper_pass) {
             // ERROR
             // ebd::fn<int() &&> f2(std::cw<&Class::mem_fn_rref_callable>, ptr);
             // ASSERT_EQ(std::move(f2)(), OVL_R_REF);
+        }
+        {
+            // non-qualifier
+            int a = 42;
+            ebd::fn<int(int)> f1(std::cw<&ebd_test_free_func_iii_add>, a);
+            ASSERT_EQ(f1(1), 43);
+
+            ebd::fn<int(int)> f2(std::cw<[](int a, int b) { return a + b; }>, a);
+            ASSERT_EQ(f2(2), 44);
+
+            ebd::fn<int(int)> f3(std::cw<+[](int a, int b) { return a + b; }>, a);
+            ASSERT_EQ(f3(3), 45);
+        }
+        {
+            // const-qualifier
+            int a = 42;
+            ebd::fn<int(int) const> f1(std::cw<&ebd_test_free_func_iii_add>, a);
+            ASSERT_EQ(f1(1), 43);
+
+            ebd::fn<int(int) const> f2(std::cw<[](int a, int b) { return a + b; }>, a);
+            ASSERT_EQ(f2(2), 44);
+
+            ebd::fn<int(int) const> f3(std::cw<+[](int a, int b) { return a + b; }>, a);
+            ASSERT_EQ(f3(3), 45);
+        }
+        {
+            // noexcept-qualifier
+            int a = 42;
+            ebd::fn<int(int) noexcept> f1(std::cw<&func_iii_add_noexcept>, a);
+            ASSERT_EQ(f1(1), 43);
+
+            ebd::fn<int(int) noexcept> f2(std::cw<[](int a, int b) noexcept { return a + b; }>, a);
+            ASSERT_EQ(f2(2), 44);
+
+            ebd::fn<int(int) noexcept> f3(std::cw<+[](int a, int b) noexcept { return a + b; }>, a);
+            ASSERT_EQ(f3(3), 45);
+        }
+        {
+            // const & noexcept-qualifier
+            int a = 42;
+            ebd::fn<int(int) const noexcept> f1(std::cw<&func_iii_add_noexcept>, a);
+            ASSERT_EQ(f1(1), 43);
+
+            ebd::fn<int(int) const noexcept> f2(std::cw<[](int a, int b) noexcept { return a + b; }>, a);
+            ASSERT_EQ(f2(2), 44);
+
+            ebd::fn<int(int) const noexcept> f3(std::cw<+[](int a, int b) noexcept { return a + b; }>, a);
+            ASSERT_EQ(f3(3), 45);
         }
     }
 
